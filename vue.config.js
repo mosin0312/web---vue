dea7786e8d9bf68e255cd132ab177facc34698c9
mosin0312 <a0909653648@gmail.com>
2025-09-17 11:42:53 +0800

    OK

--- vue.config.js
+++ vue.config.js
@@ -13,9 +13,9 @@ module.exports = {
         changeOrigin: true
       },
 
-      //api* 走 https://localhost:7050本機
+      //api* 之前走 https://localhost:7050本機
       '/api': {
-        target: 'https://localhost:7050',
+        target: 'https://163.13.201.89:5050',
         changeOrigin: true,
         secure: false // 自簽憑證才需要 false
       },
